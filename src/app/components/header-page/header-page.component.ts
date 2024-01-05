823143498390b2aa27501cdcb2e0c2531ba07ce2
Rodrigo Ezequiel <r.ezequiel@live.com.pt>
2024-01-05 15:56:00 +0000

    Foi criado componente Serve para aparecer como header com título da página. Tem o mediaquery provisório e CSS

---
+++ src/app/components/header-page/header-page.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
+
+@Component({
+  selector: 'app-header-page',
+  templateUrl: './header-page.component.html',
+  styleUrls: ['./header-page.component.scss']
+})
+export class HeaderPageComponent implements OnInit{
+
+  pageTitle: string = '';
+
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.setPageTitle();
+  }
+
+  setPageTitle() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.pageTitle = this.activatedRoute.firstChild?.snapshot.data['title'];
+        console.log(this.pageTitle);
+      });
+  }
+}
+
+
