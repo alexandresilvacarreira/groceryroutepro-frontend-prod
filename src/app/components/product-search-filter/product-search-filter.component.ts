daf48b2b50319031618eb8cde3422ccb82a18bc6
Rodrigo Ezequiel <r.ezequiel@live.com.pt>
2024-01-09 17:40:35 +0000

    Correcção e acrescentar o header

--- src/app/components/product-search-filter/product-search-filter.component.ts
+++ src/app/components/product-search-filter/product-search-filter.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faCircle} from "@fortawesome/free-regular-svg-icons/faCircle";
 import {faCircleCheck} from "@fortawesome/free-solid-svg-icons/faCircleCheck";
 import {faCircleXmark} from "@fortawesome/free-solid-svg-icons/faCircleXmark";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {NavigationService} from "../../services/navigation.service";
+
 
 @Component({
   selector: 'app-product-search-filter',
   templateUrl: './product-search-filter.component.html',
   styleUrls: ['./product-search-filter.component.scss']
 })
-export class ProductSearchFilterComponent {
+export class ProductSearchFilterComponent implements OnInit {
+  previousRoute = '';
+
+  constructor(private navigationService: NavigationService) {
+  }
+
+  ngOnInit(): void {
+
+    this.previousRoute = this.navigationService.getPreviousRoute();
+
+  }
 
   protected readonly faCircle = faCircle;
   protected readonly faCircleCheck = faCircleCheck;
   protected readonly faCircleXmark = faCircleXmark;
+  protected readonly faArrowLeft = faArrowLeft;
+
 }
