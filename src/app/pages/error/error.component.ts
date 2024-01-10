8b05881d25391f71c96ef1bd67291a55e7b39caf
Pedro Yen <phsy09@hotmail.com>
2024-01-10 18:35:12 +0000

    Pagina de erro

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {Component, OnInit} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  styleUrls: ['./error.component.scss'],
+  animations: [
+    trigger('dashAnimation1', [
+      state("appear", style({transform:"translateX(10px)", opacity: 0})),
+      state("start", style({opacity: 1})),
+      state("end", style({transform:"translateX(-50px)", opacity: 0, transformOrigin: 'center'})),
+      transition('*=>*', [animate('300ms 100ms linear')])
+    ]),
+    trigger('dashAnimation2', [
+      state("appear", style({transform:"translateX(10px)", opacity: 0})),
+      state("start", style({opacity: 1})),
+      state("end", style({transform:"translateX(-50px)", opacity: 0, transformOrigin: 'center'})),
+      transition('*=>*', [animate('200ms linear')])
+    ]),
+    trigger('dashAnimation3', [
+      state("appear", style({transform:"translateX(10px)", opacity: 0})),
+      state("start", style({opacity: 1})),
+      state("end", style({transform:"translateX(-50px)", opacity: 0, transformOrigin: 'center'})),
+      transition('*=>*', [animate('300ms 200ms linear')])
+    ])
+  ]
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit{
+
+    protected readonly faArrowLeft = faArrowLeft;
+  state: string = "appear";
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      switch (this.state) {
+        case 'appear':
+          this.state = 'start';
+          break;
+        case 'start':
+          this.state = 'end';
+          break;
+        case 'end':
+          this.state = 'appear';
+          break;
+      }
+    }, 200)
+
+  }
 
 }
