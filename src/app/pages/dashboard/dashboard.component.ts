fe5919fed292db68f62242747dc80f968082807c
Alexandre <alex.s.carreira@gmail.com>
2024-01-22 12:47:25 +0000

    Ajustes no layout do dashboard.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {User} from "../../interfaces";
+import {ShoppingList, User} from "../../interfaces";
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
 import {lastValueFrom} from "rxjs";
+import {isUser} from "../../utils";
+import {faCirclePlus} from "@fortawesome/free-solid-svg-icons/faCirclePlus";
+import {faCartShopping} from "@fortawesome/free-solid-svg-icons/faCartShopping";
+import {faCoins} from "@fortawesome/free-solid-svg-icons/faCoins";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,18 +15,20 @@ import {lastValueFrom} from "rxjs";
 })
 export class DashboardComponent implements OnInit {
 
+  shoppingList!:ShoppingList;
+
   constructor(private userService: UserService, private router: Router) {
   }
 
   ngOnInit(): void {
-    // this.user = this.userService.getCurrentUser();
-    // if (!this.user){
-    //   this.router.navigate(["/login"]);
-    // }
+    this.userService.getCurrentUser().subscribe(user=>{
+      if (isUser(user)){
+        this.shoppingList = user.currentShoppingList;
+      }
+    })
   }
 
-  isUser(obj: any): obj is User {
-    return obj !== false && obj !== null && typeof obj === 'object' && 'name' in obj;
-  }
-
+  protected readonly faCirclePlus = faCirclePlus;
+  protected readonly faCartShopping = faCartShopping;
+  protected readonly faCoins = faCoins;
 }
